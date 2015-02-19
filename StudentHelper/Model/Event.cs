b6422708179b25d3768183cc2883b3b0ef8d76aa
fred3760 <fred3760@edu.eal.dk>
2015-02-19 11:16:27 +0100

    Event.WeekNr commit

---
+++ StudentHelper/Model/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHelper.Model
+{
+    class Event
+    {
+        public int WeekNr { get; set; }
+
+    }
+}
