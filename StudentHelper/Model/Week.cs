2f04204c578c9ec86d4ec196fff0b90fc5a1bd7c
fred3760 <fred3760@edu.eal.dk>
2015-02-19 12:52:31 +0100

    Model.Week

---
+++ StudentHelper/Model/Week.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHelper.Model
+{
+    class Week
+    {
+        public List<Event> Monday { get; set; }
+        public List<Event> Tuesday { get; set; }
+        public List<Event> Wednesday { get; set; }
+        public List<Event> Thursday { get; set; }
+        public List<Event> Friday { get; set; }
+        public List<Event> Saturday { get; set; }
+        public List<Event> Sunday { get; set; }
+        public int WeekNr { get; set; }
+
+        public Week(int weeknr)
+        {
+            WeekNr = weeknr;
+        }
+    }
+}
